31f5e6cd3b2f54b1caa7105340df8392fa592aca
v0lliner <61630748+v0lliner@users.noreply.github.com>
2025-07-04 10:41:30 +0300

    Updated CookieConsentBanner.jsx

--- src/components/UI/CookieConsentBanner.jsx
+++ src/components/UI/CookieConsentBanner.jsx
@@ -198,7 +198,7 @@ const CookieConsentBanner = () => {
           bottom: 24px;
           left: 24px;
           right: 24px;
-          max-width: 500px;
+          max-width: 380px;
           margin: 0 auto;
           background-color: white;
           border-radius: 8px;
