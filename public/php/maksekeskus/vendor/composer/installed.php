49fb1d1b7a5ba298c91df9acbf5228b79d623619
v0lliner <61630748+v0lliner@users.noreply.github.com>
2025-07-06 15:09:09 +0300

    Added autoload_static.php

---
+++ public/php/maksekeskus/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'maksekeskus/maksekeskus-php',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '070fc86a931da718f340c37e22c5b20fc079cff9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'maksekeskus/maksekeskus-php' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '070fc86a931da718f340c37e22c5b20fc079cff9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'nategood/httpful' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ea02c8aedef70b9a453ce97978d9d7c413972f15',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nategood/httpful',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
