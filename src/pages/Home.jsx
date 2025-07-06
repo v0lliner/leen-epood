e00718b4d80378a5352855cf29756f1be62e3858
v0lliner <61630748+v0lliner@users.noreply.github.com>
2025-07-06 19:13:18 +0300

    Updated Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -166,7 +166,7 @@ const Home = () => {
         .hero-image img {
           width: 100%;
           height: auto;
-          object-fit: inherit;
+          object-fit: initial;
           aspect-ratio: 4/5;
           border-radius: 4px;
           display: block;
