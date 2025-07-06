c9b8b9d176c045e21dc9b32c7a2d940d53d37e74
v0lliner <61630748+v0lliner@users.noreply.github.com>
2025-07-06 19:17:13 +0300

    Updated Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -158,7 +158,7 @@ const Home = () => {
 
         .hero-image {
           width: 90%;
-          max-width: 600px;
+          max-width: 900px;
           border-radius: 8px;
           overflow: hidden; 
         }
