4d842d37949f50bed1cfd354a29843af5678f369
v0lliner <61630748+v0lliner@users.noreply.github.com>
2025-07-06 19:35:01 +0300

    Updated Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -153,7 +153,7 @@ const Home = () => {
           flex: 1;
           display: flex;
           justify-content: center;
-          align-items: center-right;
+          align-items: center;
         }
 
         .hero-image {
