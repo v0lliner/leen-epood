be2db0bcf072a162bb3a8b7de1d4a115e0a08c91
v0lliner <61630748+v0lliner@users.noreply.github.com>
2025-07-09 19:33:33 +0300

    Updated OrderNotesForm.jsx

--- src/components/Checkout/OrderNotesForm.jsx
+++ src/components/Checkout/OrderNotesForm.jsx
@@ -21,7 +21,7 @@ const OrderNotesForm = ({ formData, onChange }) => {
       <style jsx>{`
         .order-notes-form {
           margin-bottom: 32px;
-          margin-top: -48px;
+          margin-top: -44px;
         }
         
         .form-group {
