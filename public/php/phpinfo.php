329bd9ed7fd5b888a10819008fac0c54bb2f86ad
v0lliner <61630748+v0lliner@users.noreply.github.com>
2025-07-09 20:42:18 +0300

    Kontrolli PHP versiooni

---
+++ public/php/phpinfo.php
@@ -0,0 +1,10 @@
+<?php
+// Lihtne PHP versiooni kontroll
+echo "PHP versioon: " . phpversion();
+echo "<br>PHP_VERSION_ID: " . PHP_VERSION_ID;
+echo "<br>PHP_VERSION: " . PHP_VERSION;
+echo "<hr>";
+
+// Kuva täielik PHP info
+phpinfo();
+?>
