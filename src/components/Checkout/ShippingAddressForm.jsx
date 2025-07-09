d7be1e13e1b1d2558c7cfadb250b92e4e37bb468
v0lliner <61630748+v0lliner@users.noreply.github.com>
2025-07-09 19:56:41 +0300

    Updated ShippingAddressForm.jsx

--- src/components/Checkout/ShippingAddressForm.jsx
+++ src/components/Checkout/ShippingAddressForm.jsx
@@ -147,7 +147,9 @@ const ShippingAddressForm = ({
                 className={`form-input ${validationErrors.omnivaParcelMachineId ? 'has-error' : ''}`}
                 disabled={loadingParcelMachines}
               >
-                <option value="">{t('checkout.shipping.omniva.select_placeholder')}</option>
+                {!formData.omnivaParcelMachineId && (
+                  <option value="">{t('checkout.shipping.omniva.select_placeholder')}</option>
+                )}
                 {parcelMachines.map(machine => (
                   <option key={machine.id} value={machine.id}>
                     {machine.name}
